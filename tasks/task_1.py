dde9507b43abcafb578fdad476d5702f24491772
mkrakovsky <michal.krakovsky@blindspot.ai>
2025-03-11 18:45:46 +0100

    style & srp

---
+++ tasks/task_1.py
@@ -0,0 +1,47 @@
+"""
+Task1 - SRP
+
+Predstavte si systém pre malé internetové kníhkupectvo. Na začiatku je v kóde jedna trieda,
+ktorá spracováva niekoľko úloh: správu zásob kníh, spracovanie predajných transakcií a generovanie správ o predaji.
+Táto trieda porušuje princíp jedinej zodpovednosti, pretože má viac ako jeden dôvod na zmenu.
+
+Prepracujte nasledujúcu triedu jazyka Python tak, aby dodržiavala princíp jedinej zodpovednosti.
+Trieda v súčasnosti spracúva správu zásob, predajné transakcie a vytváranie správ,
+ktoré by mali byť oddelené do samostatných tried.
+"""
+
+
+class BookStore:
+    def __init__(self):
+        self.inventory = {}
+        self.sales = []
+
+    def add_book(self, title, quantity):
+        if title in self.inventory:
+            self.inventory[title] += quantity
+        else:
+            self.inventory[title] = quantity
+
+    def sell_book(self, title, quantity):
+        if title in self.inventory and self.inventory[title] >= quantity:
+            self.inventory[title] -= quantity
+            self.sales.append((title, quantity))
+        else:
+            print("Book out of stock or not enough quantity.")
+
+    def get_inventory(self):
+        return self.inventory
+
+    def get_sales_report(self):
+        report = "Sales Report:\n"
+        for sale in self.sales:
+            report += f"Title: {sale[0]}, Quantity: {sale[1]}\n"
+        return report
+
+
+"""
+V ideálnom prípade definujte samostatné triedy, ktoré budú
+1 - starať sa o inventár (pridávanie, odstraňovanie)
+2 - starať sa o proces predaja (predaj kníh, uchovávanie údajov o predaji).
+3 - starať sa o vykazovanie predaja (generovanie prehľadu o predaji).
+"""
