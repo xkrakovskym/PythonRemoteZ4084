7f42e4e86840cd7de53a136fcf4b176f29d9bb23
mkrakovsky <michal.krakovsky@blindspot.ai>
2025-03-12 19:49:32 +0100

    decorator function

---
+++ decorator_function.py
@@ -0,0 +1,21 @@
+import functools
+
+
+def my_decorator(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print("Before calling the func")
+
+        result = func(*args, **kwargs)
+
+        print("After calling func")
+
+        return result
+    return wrapper
+
+
+@my_decorator
+def say_hello(name):
+    print(f"Hello {name}!")
+
+say_hello("Michal")
